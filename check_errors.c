b775ff0a8f225af66be868211cae1c195c30abc9
Wilds Rincon <1588@holbertonschool.com>
2020-05-14 13:02:11 -0500

    Adding changes file check_errors.c

--- check_errors.c
+++ check_errors.c
@@ -36,3 +36,36 @@ void error_out(int error_code, ...)
 	free_nodes();
 	exit(EXIT_FAILURE);
 }
+
+/**
+ * others_error - handles errors.
+ * @error_code: The error codes are the following:
+ */
+void others_error(int error_code, ...)
+{
+	va_list ag;
+	char *opcode_error;
+	int l_num;
+	va_start(ag, error_code);
+	switch (error_code)
+	{
+		case 6:
+			fprintf(stderr, "L%d: can't pint, stack empty\n",
+					va_arg(ag, int));
+			break;
+		case 7:
+			fprintf(stderr, "L%d: can't pop an empty stack\n",
+					va_arg(ag, int));
+			break;
+		case 8:
+			l_num = va_arg(ag, unsigned int);
+			opcode_error = va_arg(ag, char *);
+			fprintf(stderr, "L%d: can't %s, stack too short\n", l_num, opcode_error);
+			break;
+		default:
+			break;
+	}
+	free_nodes();
+	exit(EXIT_FAILURE);
+}
+
