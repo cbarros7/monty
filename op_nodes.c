af63ae6088f34c414266d4f61715ad160d30bb23
cbarros7 <cj.barros07@gmail.com>
2020-05-14 17:46:50 -0500

    Fix error Betty

--- op_nodes.c
+++ op_nodes.c
@@ -1,34 +1,40 @@
 #include "monty.h"
 
 /**
- * swap_nodes - Swap element 1 for element 2 of the stack
+ * swap - Swap element 1 for element 2 of the stack
  * @stack: points to the top node of the stack.
  * @line_num: opcode line number.
  */
 void swap(stack_t **stack, unsigned int line_num)
 {
 	stack_t *tmp;
+
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
-		more_err(8, line_num, "swap");
+		others_error(8, line_num, "swap");
+
 	tmp = (*stack)->next;
 	(*stack)->next = tmp->next;
+
 	if (tmp->next != NULL)
 		tmp->next->prev = *stack;
+
 	tmp->next = *stack;
 	(*stack)->prev = tmp;
 	tmp->prev = NULL;
 	*stack = tmp;
 }
 /**
- * add_nodes - Adds the top two elements of the stack.
+ * add - Adds the top two elements of the stack.
  * @stack: Pointer to a pointer pointing to top node of the stack.
  * @line_num: Interger representing the line number of of the opcode.
  */
 void add(stack_t **stack, unsigned int line_num)
 {
 	int sum;
+
 	if (stack == NULL || *stack == NULL || (*stack)->next == NULL)
-		more_err(8, line_num, "add");
+		others_error(8, line_num, "add");
+
 	(*stack) = (*stack)->next;
 	sum = (*stack)->n + (*stack)->prev->n;
 	(*stack)->n = sum;
